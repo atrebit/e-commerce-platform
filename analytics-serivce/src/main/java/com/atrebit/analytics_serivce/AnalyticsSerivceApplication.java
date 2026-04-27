1a361f4434acb681962a8b40c1fa41b2510b8366
atrebit <test.testmail@test.com>
2026-04-27 22:16:49 +0200

    generate analytics-service

---
+++ analytics-serivce/src/main/java/com/atrebit/analytics_serivce/AnalyticsSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.atrebit.analytics_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsSerivceApplication.class, args);
+	}
+
+}
---
+++ analytics-serivce/src/test/java/com/atrebit/analytics_serivce/AnalyticsSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atrebit.analytics_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
