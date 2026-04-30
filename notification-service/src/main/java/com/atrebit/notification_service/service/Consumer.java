9b88686a93e9c2e057b490c1503e9815106f003f
atrebit <test.testmail@test.com>
2026-04-30 21:51:30 +0200

    consume rabbitmq messages

---
+++ notification-service/src/main/java/com/atrebit/notification_service/service/Consumer.java
@@ -0,0 +1,14 @@
+package com.atrebit.notification_service.service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class Consumer{
+    @RabbitListener(queues = {"${application.order-queue}"})
+    public void consume(String message){
+        log.info(String.format("sending notification with content: %s", message));
+    }
+}
