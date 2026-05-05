771caeaeb48f9d9a96eb7b737a5f4039b1360296
atrebit <test.testmail@test.com>
2026-05-05 18:49:23 +0200

    subscribe inventory-service to orders-topic

---
+++ inventory-service/src/main/java/com/atrebit/inventory_service/config/RabbitMQConfig.java
@@ -0,0 +1,42 @@
+package com.atrebit.inventory_service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+    
+    @Value("${application.order-queue}")
+    public String queue;
+
+    @Value("${application.order-exchange}")
+    public String exchange;
+
+    @Value("${application.order-routing-key}")
+    public String routingKey;
+
+    @Bean
+    public Queue ordersCreatedQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public TopicExchange ordersExchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(ordersCreatedQueue())
+                .to(ordersExchange())
+                .with(routingKey);
+    }
+
+}
